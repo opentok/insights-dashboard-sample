6505f2e6b752b466c78659dd5d4b2c9d2288b765
Jeff Swartz <jeff.swartz@vonage.com>
2019-11-12 17:55:19 -0800

    Update Publisher and Subscriber minutes by Session to use pagination

--- src/charts/UsageBySession.js
+++ src/charts/UsageBySession.js
@@ -3,9 +3,12 @@ import gql from 'graphql-tag';
 import { withApollo } from 'react-apollo';
 import { get, map } from 'lodash';
 import moment from 'moment';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Link from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
@@ -16,12 +19,18 @@ import round from './helpers/round';
 const apiKey = process.env.REACT_APP_API_KEY;
 
 /* Get all session IDs from the last 10 days */
-const sessionSummariesQuery = gql`
+const sessionSummariesQuery = endCursor => gql`
   {
     project(projectId: ${apiKey}) {
       sessionData {
-        sessionSummaries(start: ${moment().subtract(10, 'days')}) {
+        sessionSummaries(
+          start: ${moment().subtract(10, 'days')}
+          endCursor: "${endCursor}"
+        ) {
           totalCount
+          pageInfo {
+            endCursor
+          }
           resources {
             sessionId
           }
@@ -57,30 +66,44 @@ class UsageBySession extends Component {
     this.state = {
       sessionsInfo: [],
       loading: true,
+      loadingMore: false,
+      endCursor: '',
+      totalCount: 0,
     }
   }
   getSessions = async () => {
-    const query = { query: sessionSummariesQuery };
+    const query = { query: sessionSummariesQuery(this.state.endCursor) };
     const results = await this.props.client.query(query);
+    const { totalCount, pageInfo } = results.data.project.sessionData.sessionSummaries;
+    this.setState({
+      endCursor: pageInfo.endCursor || '',
+      totalCount,
+    });
     return get(results.data, 'project.sessionData.sessionSummaries.resources', []);
   }
-  getSessionsInfo = async (sessionIds) => {
+  getSessionsInfo = async () => {
+    const sessionIds = map(await this.getSessions(this.state.endCursor), (session) => `"${session.sessionId}"`);
     const query = { query: sessionQuery(sessionIds) };
     const results = await this.props.client.query(query);
-    return get(results.data, 'project.sessionData.sessions.resources', []);
-  }
-  async componentDidMount() {
-    const sessionIds = map(await this.getSessions(), (session) => `"${session.sessionId}"`);
-    const sessionsInfo = await this.getSessionsInfo(sessionIds);
+    const sessionsInfo = get(results.data, 'project.sessionData.sessions.resources', []);
     this.setState({
-      sessionsInfo,
-      loading: false,
+      sessionsInfo: this.state.sessionsInfo.concat(sessionsInfo),
     });
+  }
+  
+  async componentDidMount() {
+    await this.getSessionsInfo();
+    this.setState({ loading: false });
   }
   render() {
     const { sessionsInfo, loading } = this.state;
     if (loading) return <Loading />;
     if (sessionsInfo.length === 0) return <NoResultsFound />;
+    const handleNext = async () => {
+      this.setState({ loadingMore: true });
+      await this.getSessionsInfo();
+      this.setState({ loadingMore: false });
+    }
     return (
       <Paper>
         <Table>
@@ -93,7 +116,7 @@ class UsageBySession extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            { sessionsInfo.map(({sessionId, meetings, publisherMinutes, subscriberMinutes}) => (
+            { this.state.sessionsInfo.map(({sessionId, meetings, publisherMinutes, subscriberMinutes}) => (
               <TableRow key={sessionId}>
                 <TableCell component="th" scope="row">{sessionId}</TableCell>
                 <TableCell align="right" scope="row">{meetings.totalCount}</TableCell>
@@ -102,6 +125,24 @@ class UsageBySession extends Component {
               </TableRow>
             ))}
           </TableBody>
+          <TableFooter style={{ padding: "50px" }}>
+            <TableRow>
+              <TableCell>
+                Showing {this.state.sessionsInfo.length} of {this.state.totalCount} results.
+                <Link
+                  onClick={handleNext}
+                  size="small"
+                  color="primary"
+                  disabled={!this.state.endCursor || this.state.loadingMore}
+                >
+                  Retrieve more
+                </Link>
+                {this.state.loadingMore &&
+                  <CircularProgress size="20px"/>
+                }
+              </TableCell>
+            </TableRow>
+          </TableFooter>
         </Table>
       </Paper>
     );
