4976cbf6e125da7b6f1af5507ab7b3c75c467a02
Raulkg <gaddam.rahul.kumar@gmail.com>
2021-03-18 13:24:51 -0700

    ppm graph added
    date filter added

--- src/charts/UsageByParticipantTier.js
+++ src/charts/UsageByParticipantTier.js
@@ -4,73 +4,148 @@ import { Query } from 'react-apollo';
 import { Line } from 'react-chartjs-2';
 import { get } from 'lodash';
 import moment from 'moment';
+import MomentUtils from "@date-io/moment";
 import Loading from '../components/Loading';
 import ErrorMessage from '../components/ErrorMessage';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 const apiKey = process.env.REACT_APP_API_KEY;
 
-const query = gql`
-  {
-    project(projectId: ${apiKey}) {
-      projectData(
-        start: ${moment().subtract(10, 'months')},
-        interval: DAILY
-      ) {
-        resources {
-          intervalStart,
-          intervalEnd,
-          usage {
-          participantMinutes{
-            from1To2Publishers
-            from3To6Publishers
-            from7To8Publishers
-            from1To4Publishers
-            from5To8Publishers
-            from1To8Publishers
-            from1To10Publishers
-            from9To10Publishers
-            from11To35Publishers
-            from11To20Publishers
-            from11To35Publishers
-            from36PlusPublishers
-            from21To35Publishers
-            from36To40Publishers
-            from41PlusPublishers
-          }
+const ppmDisplaySettings = [
+ {
+    label: '1 - 2 publishers',
+    backgroundColor: '#06ba77',
+    key: 'from1To2Publishers'
+  },
+{
+    label: '3 - 6 publishers',
+    backgroundColor: '#9941ff',
+    key: 'from3To6Publishers'
+  },
+{
+    label: '7 - 8 publishers',
+    backgroundColor: '#fa7454',
+    key: 'from7To8Publishers'
+  },
+{
+    label: '9 - 10 publishers',
+    backgroundColor: '#d6219c',
+    key: 'from9To10Publishers'
+  },
+{
+    label: '11 - 35 publishers',
+    backgroundColor: '#003f5f',
+    key: 'from11To35Publishers'
+  },
+{
+    label: '> 35 publishers',
+    backgroundColor: '#e84545',
+    key: 'from36PlusPublishers'
+  }
+];
+
+class UsageByParticipantTier extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment(),
+      endDate: moment()
+    }
+    this.setStartDate = this.setStartDate.bind(this);
+    this.setEndDate = this.setEndDate.bind(this);
+  }
+
+  setStartDate(date) {
+    this.setState({
+        startDate: date
+    })
+  }
+
+  setEndDate(date) {
+      this.setState({
+          endDate:date
+      })
+  }
+
+  render() {
+  const { startDate,endDate } = this.state;
+  let formattedPPM= '';
+  ppmDisplaySettings.map( ppm => formattedPPM += `${ppm.key}\n`);
+  const query = gql`
+    {
+      project(projectId: ${apiKey}) {
+        projectData(
+          start: ${moment(startDate)},
+          end:  ${moment(endDate)},
+          interval: DAILY
+        ) {
+          resources {
+            intervalStart,
+            intervalEnd,
+            usage {
+            participantMinutes{
+                 ${formattedPPM}
+            }
+            }
           }
         }
       }
     }
-  }
-`;
+  `;
 
-class UsageByParticipantTier extends Component {
-  render() {
     return (
-      <Query query={query}>
+    <div>
+     <MuiPickersUtilsProvider libInstance={moment} utils={MomentUtils}>
+         <KeyboardDatePicker
+           disableToolbar
+           variant="inline"
+           format="MM/DD/yyyy"
+           margin="normal"
+           id="date-picker-inline"
+           label="Start Date"
+           value={startDate}
+           onChange={this.setStartDate}
+          />
+           <KeyboardDatePicker
+             disableToolbar
+             variant="inline"
+             format="MM/DD/yyyy"
+             margin="normal"
+             id="date-picker-inline"
+             label="End Date"
+             value={endDate}
+             onChange={this.setEndDate}
+            />
+     </MuiPickersUtilsProvider>
+      <Query key={`qid-${moment(startDate).valueOf()}`} query={query}>
         {({ loading, error, data }) => {
           if (loading) return <Loading />;
           if (error) return <ErrorMessage error={error.message} />;
           const resources = get(data, 'project.projectData.resources', []);
+          const dataSetConfig = ppmDisplaySettings
+                                      .map(ppm =>{
+                                          ppm.data = resources.map(item =>
+                                                    get(item, `usage.participantMinutes.${ppm.key}`,0))
+                                          return ppm;
+                                      })
           return (
             <Line data={{
               labels: resources.map(item => moment(item.intervalStart).format('MMM DD')),
-              datasets: [
-              {
-                        label: 'from1To2Publishers',
-                        backgroundColor: 'rgba(75,192,192,0.4)',
-                        data: resources.map(item => get(item, 'usage.participantMinutes.from1To2Publishers', 0)),
-              },
-                {
-                          label: 'from1To4Publishers',
-                          backgroundColor: '#36A2EB',
-                          data: resources.map(item => get(item, 'usage.participantMinutes.from1To4Publishers', 0)),
-                }
-              ],
-            }} />
+              datasets: dataSetConfig,
+
+            }}
+           options= {{
+                   scales: {
+                       yAxes: [{
+                           stacked: true
+                       }]
+                   }
+               }}
+             />
           );
         }}
       </Query>
+      </div>
     );
   }
 }
